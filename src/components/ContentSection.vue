b0f10daa0e5a93a7da5d4254e647808ca7f37d94
magvtv <magutujr@gmail.com>
2024-07-16 12:43:41 +0300

    added section in-code, need to transfer it to assets/content

--- src/components/ContentSection.vue
+++ src/components/ContentSection.vue
@@ -1,20 +1,19 @@
 <template>
-    <section class="py-16">
-        <div class="container mx-auto flex flex-wrap">
-            <div class="w-full md:w-1/2 p-5">
-                <h2 class="text-3xl font-bold mb-5">
-                    {{ title }}
-                </h2>
-                <p class="text-lg mb-5">
-                    {{ details }}
-                </p>
-                <a href="" class="capitalize text-blue-500 hover:underline">
-                    read more
-                </a>
-            </div>
-
-            <div class="w-full md:w-1/2 p-5">
-                <img :src="image" :alt="altText">
+    <section id="content" class="bg-gray-900 py-20 text-white">
+        <div class="container mx-auto px-5">
+            <div class="grid md:grid-cols-2 gap-12">
+                <div v-for="(section, index) in sections" :key="index" class="flex flex-col items-center">
+                    <img :src="section.image" alt="" class="w-full h-64 object-cover mb-5">
+                    <h2 class="text-2xl font-bold">
+                        {{ section.title }}
+                    </h2>
+                    <p class="mt-4">
+                        {{ section.details }}
+                    </p>
+                    <a href="#" class="mt-5 text-lg underline">
+                        Read more
+                    </a>
+                </div>
             </div>
         </div>
     </section>
@@ -24,11 +23,26 @@
 
 export default {
     name: 'ContentSection',
-    props: {
-        title: String,
-        details: String,
-        image: String,
-        altText: String
+    data() {
+        return {
+            sections: [
+                {
+                    image: '',
+                    title: 'What Level of Hiker are You',
+                    details: 'Determining what level of hiker you are can be an important tool when planning future hikes',
+                },
+                {
+                    image: '',
+                    title: 'Picking The Right Hiking Gear',
+                    details: 'The nice thing about beginning a hiking experience is that you do not really need a gear',
+                },
+                {
+                    image: '',
+                    title: 'Understand your Map & Timing',
+                    details: 'To start, print out the hiking guide and the map. If it is raining, throw them in a zip-lock bag',
+                }
+            ]
+        }
     }
 }
 
