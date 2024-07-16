fb1dcb8bbe6c5cc10fd65b3e42e7bb82cde4730b
magvtv <magutujr@gmail.com>
2024-07-16 15:01:50 +0300

    added backdrop image

--- src/components/MainHero.vue
+++ src/components/MainHero.vue
@@ -1,11 +1,11 @@
 <template>
-    <section class="relative h-screen bg-cover bg-center" style="background-image: url('../assets/images/hero-background.jpg');">
-        <div class="absolute inset-0 bg-black bg-opacity-80">
+    <section class="hero relative h-screen bg-cover bg-center">
+        <div class="absolute inset-0 bg-black bg-opacity-10">
             <div class="relative z-1- flex flex-col justify-center items-center h-full text-center text-white">
                 <p class="uppercase mt-4">
                     your hiking guide
                 </p>
-                <h1 class="text-5xl font-bold mb-5 capitalize">
+                <h1 class="text-5xl font-bold mb-5 px-4 capitalize">
                     be prepared for the mountains and beyond
                 </h1>
                 <a href="#content" class="mt-6 text-lg hover:underline capitalize animate-bounce">
