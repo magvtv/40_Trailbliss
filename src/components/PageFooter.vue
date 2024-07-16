7ab2971956bd7b51744a0c6b89c6e6ae1b7dfb00
magvtv <magutujr@gmail.com>
2024-07-16 12:44:07 +0300

    better looking footer

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,29 +1,72 @@
 <template>
-    <footer class="bg-gray-900 text-white p-5">
-        <div class="container mx-auto text-center">
-            <span class="capitalize text-lg">
-                trailbliss
-            </span>
-            <p>
-                Get out there and discover your next slope, mountain and destination!
-            </p>
+    <footer class="bg-gray-900 text-white py-10">
+        <div class="container mx-auto px-6">
+            <div class="flex justify-between items-center">
+                <div>
+                    <span class="capitalize text-lg">
+                        trailbliss
+                    </span>
+                    <p>
+                        Get out there and discover your next slope, mountain and destination!
+                    </p>
+                    <p class="mt-4">
+                        © 2024 Trailbliss, Inc. Terms &amp; Privacy
+                    </p>
 
-            <div class="mt-4">
-                <a href="/about" class="mr-4">
-                    about us
-                </a>
-                <a href="/adventures" class="mr-4">
-                    adventures
-                </a>
-                <a href="/contact" class="mr-4">
-                    contact
-                </a>
-                <a href="/policy" class="mr-4">
-                    privacy policy
-                </a>
+                </div>
+
+                <div>
+                    <p>More about Trailbliss</p>
+                    <ul>
+                        <li class="capitalize">
+                            <a href="#">
+                                our adventures
+                            </a>
+                        </li>
+                        <li class="capitalize">
+                            <a href="#">
+                                meet fellow hikers
+                            </a>
+                        </li>
+                        <li class="capitalize">
+                            <a href="#">
+                                our adventures
+                            </a>
+                        </li>
+                        <li class="capitalize">
+                            <a href="#">
+                                meet fellow hikers
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div>
+                    <p>More about Trailbliss</p>
+                    <ul>
+                        <li class="capitalize">
+                            <a href="#">
+                                our adventures
+                            </a>
+                        </li>
+                        <li class="capitalize">
+                            <a href="#">
+                                meet fellow hikers
+                            </a>
+                        </li>
+                        <li class="capitalize">
+                            <a href="#">
+                                our adventures
+                            </a>
+                        </li>
+                        <li class="capitalize">
+                            <a href="#">
+                                meet fellow hikers
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-
-            <p class="mt-4">© 2024 Trailbliss, Inc. Terms & Privacy</p>
         </div>
     </footer>
 </template>
